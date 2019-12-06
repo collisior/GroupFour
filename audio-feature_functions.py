9e868bea45c80a44fc61ca25b3b86f36eaff7320
DanielMagat <39805120+DanielMagat@users.noreply.github.com>
2019-12-06 00:37:59 -0500

    Method for obtaining audio features of multiple tracks at once added.

--- audio-feature_functions.py
+++ audio-feature_functions.py
@@ -1,3 +1,7 @@
 def track_audio_features(spotify, tid):
     trackuri = spotify._get_uri('track', tid)
     return spotify._get("audio-features/%s/track" %trackuri)
+
+def multiple_tracks_audio_features(spotify, tracks):
+    ftracks = [spotify._get_uri('track', tid) for tid in tracks]
+    return spotify._get("audio-features" % ftracks)
