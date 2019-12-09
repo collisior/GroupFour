b11187c4d3a0d01acea9b4da6a3be187396e5021
Camilla Satte <camillasatte@gmail.com>
2019-12-09 18:31:06 -0500

    without db

--- prototype 2.0/app.py
+++ prototype 2.0/app.py
@@ -12,19 +12,19 @@ from flask_migrate import Migrate, MigrateCommand
 
 
 app = Flask(__name__, static_folder="images")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.secret_key = os.urandom(24)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.secret_key = os.urandom(24)
+#
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+#
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    token = db.Column(db.String())
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(128))
+#     token = db.Column(db.String())
 
 # app.secret_key = b'any random string'
 
