a464f31c59be08248eacdd8fb17cc36031c6386c
Camilla Satte <camillasatte@gmail.com>
2019-12-09 22:57:29 -0500

    new stuff

--- prototype 2.0/app.py
+++ prototype 2.0/app.py
@@ -1,38 +1,25 @@
 """This script builds the Flask app."""
+from datetime import datetime
 import spotipy
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, escape
 import spotipy.oauth2 as oauth2
 from functions import authorize, initializer, get_playlist, logger, get_current_user_saved_tracks
 from secret import SCOPE, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 from azure_functions import get_face_emotion_information
 import os
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_script import Manager
-#from flask_migrate import Migrate, MigrateCommand
+from flask_sqlalchemy import SQLAlchemy
 
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "moodify.db"))
 
 app = Flask(__name__, static_folder="static")
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# app.secret_key = os.urandom(24)
-#
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-#
-# manager = Manager(app)
-# manager.add_command('db', MigrateCommand)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(128))
-#     token = db.Column(db.String())
-
-# app.secret_key = b'any random string'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+db = SQLAlchemy(app)
 
 def flask_app():
-
-    app = Flask(__name__, static_folder="static")
-    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-    token = ''
 
     @app.route('/', methods=['GET'])
     def index():
@@ -44,25 +31,29 @@ def flask_app():
         sp_oauth = oauth2.SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, scope=SCOPE)
         return redirect(sp_oauth.get_authorize_url())
 
+    @app.route('/logout', methods = ['GET'])
+    def logout():
+        logger('button', 'logout')
+        # session.clear()
+        # print("SESSION POP  ",session.pop('username', None))
+        return render_template('index.html')
 
     @app.route('/authorization', methods=['GET','POST'])
     def authorization():
         response = request.url
         username = authorize(response)
         logger(username, 'attempt')
-        spotify_object =initializer(username)
-        # session['spotify_object'] = spotify_object
-        # get_playlist(spotify_object, 'happiness', 10)
+        session['username'] = username
         logger(username, 'success')
         return render_template('upload.html')
 
 
     @app.route("/upload", methods=["POST","GET"])
     def upload():
-
-        spotify_object = initializer('camillasatte')
+        username = session.pop('username', None)
+        print(username, " 0000000 \n\n\n\n")
+        spotify_object = initializer(username)
         print('\n\n\n\n\n')
-        print("TYPE:", type(spotify_object))
 
         num_of_tracks = 10
         target = os.path.join(APP_ROOT, 'images/')
@@ -81,11 +72,12 @@ def flask_app():
             print("Save it to:", destination)
             upload.save(destination)
             emotion = get_face_emotion_information(destination)
-            # spotify_object = session.get('spotify_object', None)
+            spotify_object = session.get('spotify_object', None)
             get_playlist(spotify_object, emotion, num_of_tracks)
             print("DONENENNENENE")
         # return send_from_directory("images", filename, as_attachment=True)
         return render_template("complete.html", file_path=destination)
+
 
     @app.route('/upload/<file_path>')
     def send_image(file_path):
@@ -97,5 +89,9 @@ def flask_app():
 
 
 if __name__ == '__main__':
+    print("Creating MOODIFY database...")
+    db.create_all()
+    print("MOODIFY database successfully created!")
     APP = flask_app()
+    APP.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     APP.run(debug=True)
